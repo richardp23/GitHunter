e2043cb7504ef6750441c889a23e5fafa444fad7
justincracchiolo <justincracchiolo@gmail.com>
2026-02-07 12:16:47 -0500

    UI to submit user to look up

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,29 @@
 const express = require("express"); //create express server
 const axios = require("axios"); //api calling
+const cors = require("cors"); //cross-origin resource sharing
 const app = express(); //app creation
 
-// test express backend
+app.use(cors());
 
-
-app.get("/", (req, res) => { res.send("Backend is running"); }); 
 app.listen(5000, () => { console.log("Server running on port 5000"); });
-
-//get username and make call to express server to retrieve user data. 
-//To do: retreive information from the user
-
-/* 
+ 
 app.get("/api/user/:username", async (req, res) => {
   const username = req.params.username;
 
   try {
-    const response = await axios.get(`https://api.github.com/users/${username}`);
-    res.json(response.data);
-    console.log(response.data);
+    //get user 
+    const userReponse = await axios.get(`https://api.github.com/users/${username}`);
+    const userData = userReponse.data;
+
+    //get repo data from user 
+    const repoResponse = await axios.get(userData.repos_url);
+    const repoData = repoResponse.data;
+    
+    // console.log(repoData); 
+    //user repo data
+
   } catch (err) {
     res.status(404).json({ error: "User not found" });
   }
 });
-*/
-
-
 
